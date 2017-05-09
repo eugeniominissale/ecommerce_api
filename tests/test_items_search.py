5faf94b6cc227f849f5c5920c4fd633acb8fafa0
Eugenio Minissale <eugeniominissale@gmail.com>
2017-05-09 19:46:05 +0200

    test_get_items_search__failed/success, style fixes.

--- tests/test_items_search.py
+++ tests/test_items_search.py
@@ -1,13 +1,10 @@
 from models import Item, ItemIndex
-from tests.test_utils import open_with_auth, add_user, add_address
 from tests.test_case import TestCase
-from http.client import (OK, NOT_FOUND, NO_CONTENT, BAD_REQUEST,
-                         CREATED, CONFLICT, UNAUTHORIZED)
+from http.client import OK, BAD_REQUEST
 import json
-import uuid
 
 ITEM1 = {
-    'item_id': '429994bf-784e-47cc-a823-e0c394b823e8',
+    'uuid': '429994bf-784e-47cc-a823-e0c394b823e8',
     'name': 'mario',
     'price': 20.20,
     'description': 'svariati mariii',
@@ -15,28 +12,63 @@ ITEM1 = {
 }
 
 ITEM2 = {
-    'item_id': '577ad826-a79d-41e9-a5b2-7955bcf03499',
+    'uuid': '577ad826-a79d-41e9-a5b2-7955bcf03499',
     'name': 'GINO',
     'price': 30.20,
     'description': 'svariati GINIIIII',
     'availability': 2,
 }
 
+ITEM3 = {
+    'uuid': 'f47f87c6-0de0-4dbf-9aae-841f7cdea11d',
+    'name': 'GIOVANNI',
+    'price': 10.20,
+    'description': 'svariati GIOVANNI',
+    'availability': 3,
+}
+
 
 class TestItemsSearch(TestCase):
+
+    def test_get_items_search__failed(self):
+        item1 = Item.create(**ITEM1)
+
+        ItemIndex.create(
+            uuid=item1.uuid,
+            name=item1.name,
+            description=item1.description)
+
+        query = ""
+        resp = self.app.get('/items/db', query_string=query)
+        items = json.loads(resp.data)
+
+        assert resp.status_code == BAD_REQUEST
+        assert itetms is None
 
     def test_get_items_search__success(self):
         item1 = Item.create(**ITEM1)
         item2 = Item.create(**ITEM2)
+        item3 = Item.create(**ITEM3)
 
         ItemIndex.create(
-            item_id=item1.item_id,
+            uuid=item1.uuid,
             name=item1.name,
             description=item1.description)
         ItemIndex.create(
-            item_id=item2.item_id,
+            uuid=item2.uuid,
             name=item2.name,
             description=item2.description)
+        ItemIndex.create(
+            uuid=item3.uuid,
+            name=item3.name,
+            description=item3.description)
 
-        query = "mario"
-        resp = self.app.get('/items/db', data=query)
+        query = "mariii OR GIOVANNI"
+        resp = self.app.get('/items/db', query_string=query)
+        items = json.loads(resp.data)
+
+        assert resp.status_code == OK
+        assert items[0]['uuid'] == item1.uuid
+        assert items[0]['name'] == 'mario'
+        assert items[1]['uuid'] == item3.uuid
+        assert items[1]['name'] == 'GIOVANNI'
