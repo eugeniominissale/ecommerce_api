9a11acf4bffb897520410d8f67c8f1b87733e4ff
Eugenio Minissale <eugeniominissale@gmail.com>
2017-05-09 13:51:18 +0200

    WIP get.

--- views/items_search.py
+++ views/items_search.py
@@ -1,9 +1,23 @@
 from flask import abort, request
 from flask_restful import Resource
 from http.client import (CREATED, NO_CONTENT, NOT_FOUND, OK, BAD_REQUEST, UNAUTHORIZED)
+from models import Item, ItemIndex
 
 
 class ItemsSearchHandler(Resource):
 
     def get(self):
-        return [o.json() for o in Item.select()], OK
+
+        query = str(request.args[0])
+
+        if not query:
+            return BAD_REQUEST
+
+        res = (Item.select()
+               .join(
+               ItemIndex,
+               on=(Item.item_id == ItemIndex.item_id))
+               .where(ItemIndex.match(query))
+               .order_by(ItemIndex.bm25()))
+
+        return list(res), OK
