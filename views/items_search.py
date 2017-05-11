45c92c0942478af092c9c4f6ec1d2698a1043034
Eugenio Minissale <eugeniominissale@gmail.com>
2017-05-11 11:52:58 +0200

    WIP

--- views/items_search.py
+++ views/items_search.py
@@ -2,6 +2,7 @@ from flask import abort, request
 from flask_restful import Resource
 from http.client import (CREATED, NO_CONTENT, NOT_FOUND, OK, BAD_REQUEST, UNAUTHORIZED)
 from models import Item, ItemIndex
+from peewee import SQL
 
 
 class ItemsSearchHandler(Resource):
@@ -13,13 +14,28 @@ class ItemsSearchHandler(Resource):
         if not query:
             return None, BAD_REQUEST
 
-        res = (Item.select()
-               .join(
-               ItemIndex,
-               on=(Item.uuid == ItemIndex.uuid))
-               .where(ItemIndex.match(query))
-               .order_by(ItemIndex.bm25()))
+        # 0 / -1 (best)
+        # -0.83 / -0,17 (occorrenze >, stessa colonna)
+        # -1.17 / -0.83 (1,1 / 5,0)
+        # -1.17 / -0.83 (1,1 / 0,5)
+        # -1 / -1 (1,0 /0,5)
 
+        res = (Item
+               .select(Item, ItemIndex, ItemIndex.rank().alias('score'))
+               .join(ItemIndex,
+                     on=(Item.id == ItemIndex.docid))
+               .where(ItemIndex.match(query))
+               .order_by(SQL('score').asc()))
+
+        # for item in res:
+        #     print(item.name, round(item.score, 2))
+
+        # query = ItemIndex.search_bm25(query,
+        #                               with_score=True).tuples()
+        # res = [o for o in query]
+
+        # print(res)
+        # import pdb; pdb.set_trace()
         if not res:
             return None, NOT_FOUND
         return [o.json() for o in res], OK
